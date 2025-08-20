e8d25225547ce26b5c9fde4c64d019b8471e2d7b
Lavanyacheshani <143724777+Lavanyacheshani@users.noreply.github.com>
2025-08-20 23:21:54 +0530

    add xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.planetholidaytravel.com', // Your domain
+  generateRobotsTxt: true, // also generate robots.txt file
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
